344404834483a28f1f49b9b9050b65993920e1cd
ArthurHub <thekingarthur@gmail.com>
2013-12-12 18:55:44 +0200

    * tests

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -11,12 +11,9 @@
 // "The Art of War"
 
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Drawing.Printing;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace HtmlRenderer.Demo
@@ -29,7 +26,9 @@ namespace HtmlRenderer.Demo
         [STAThread]
         private static void Main()
         {
-            // TestPrint();
+//            TestPrint();
+//            TestImage();
+//            return;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
@@ -39,97 +38,32 @@ namespace HtmlRenderer.Demo
 //            Application.Run(new PerfForm());
 
 //            PerfForm.Run();
+        }
 
-//            ObfuscateHtml();
+        private static void TestImage()
+        {
+            var html1 = "<p style='font-size:2em;color: gray'>This is an <b>HtmlLabel</b> on transparent background with <span style='color: red'>colors</span> and links: <a href='http://htmlrenderer.codeplex.com/'>HTML Renderer</a></p>";
+            var img = HtmlRender.RenderToImageGdiPlus(html1, new Size(600, 200));
+            // var img = HtmlRender.RenderToImage(html, new Size(600, 200));
+            img.Save(@"d:\test1.png", ImageFormat.Png);
+
+            var image = Image.FromFile(@"d:\b.png");
+            HtmlRender.RenderToImage(image, html1, new Point(50, 0), new Size(620, 0));
+            image.Save(@"d:\test2.png");
+
+            var html2 = "<html><body style=\"margin: 0px; height: 461px; background-image:url(d:/b.png);background-repeat:no-repeat;\"><h1 style=\" margin-top:40px;margin-left:150px;\">"
+            + "<span style=\"font-family:Monotype Corsiva;\"><em>Test HTML</em></span></h1><div style=\"width:560px;height:200px;margin-top:120px;margin-right:55px;margin-left:150px;\"><em>"
+            + "<span style=\"font-family:Monotype Corsiva;\"><span style=\"font-size:22px;\">Some Text Here</span></span></em></div></body></html>";
+            var img2 = HtmlRender.RenderToImage(html2, new Size(755, 461));
+            img2.Save(@"d:\test3.png", ImageFormat.Png);
         }
 
         private static void TestPrint()
         {
-//            string html = File.ReadAllText(@"c:\source\html.txt");
-            string html = File.ReadAllText(@"C:\Source\GitHub\HTML-Renderer\Source\Demo\Samples\02.text.htm");
-
+            var html = "<p style='font-size:3em;color: gray'>This is an <b>HtmlLabel</b> on transparent background with <span style='color: red'>colors</span> and links: <a href='http://htmlrenderer.codeplex.com/'>HTML Renderer</a></p>";
             var doc = new PrintDocument();
-            doc.PrintPage += (sender, args) =>
-                {
-                    var fact = args.Graphics.DpiX/96f;
-
-//                    args.Graphics.PageScale = 1/fact;
-
-//                    var font = new Font("Arial", 10*fact);
-//                    TextRenderer.DrawText(args.Graphics,"Hello World of PDF", font, new Point((int) (50*fact),(int) (50*fact)), Color.Red);
-
-//                    var font2 = new Font("Arial", 10);
-//                    args.Graphics.DrawString("Hello World of PDF", font2, Brushes.DarkGreen, new PointF(50, 50));
-
-//                    Point location = new Point((int) (args.MarginBounds.Location.X*fact), (int) (args.MarginBounds.Location.Y*fact));
-//                    Size maxSize = new Size((int)(args.MarginBounds.Size.Width * fact), (int) (args.MarginBounds.Size.Height*fact));
-//                    HtmlRender.Render(args.Graphics, html, args.MarginBounds.Location, args.MarginBounds.Size);
-
-                    var image = HtmlRender.RenderToImage(html, args.MarginBounds.Size);
-
-                    args.Graphics.DrawImageUnscaled(image, args.MarginBounds.Location);
-                };
-//            doc.Print();
-
-//            var bitmap = HtmlRender.RenderToImage(File.ReadAllText(@"c:\source\html.txt"), 350, 500);
-//            var bitmap = HtmlRender.RenderToImage(html, 600);
-//            bitmap.Save(@"c:\source\test.png", ImageFormat.Png);
-        }
-
-        /// <summary>
-        /// Used so html samples can be added where the content of the html is confidential.
-        /// </summary>
-        public static void ObfuscateHtml()
-        {
-            var html = GetHtmlFromResource("HtmlRenderer.Demo.PerfSamples.2.Tables.htm");
-            var sb = new StringBuilder(html);
-
-            var rand = new Random();
-
-            int idx = 0;
-            while (idx < html.Length)
-            {
-                var tagIdx = html.IndexOf('<', idx);
-                if (tagIdx < 0)
-                    tagIdx = html.Length;
-
-                for (int i = idx; i < tagIdx; i++)
-                {
-                    if(char.IsLetter(html,i))
-                    {
-                        sb[i] = Convert.ToChar(97 + rand.Next(18));
-                    }
-                }
-
-                if(tagIdx < html.Length)
-                {
-                    tagIdx = html.IndexOf('>',tagIdx);
-                    if (tagIdx < 0)
-                        tagIdx = html.Length;
-                }
-
-                idx = tagIdx;
-            }
-
-            var result = sb.ToString();
-
-            result = Regex.Replace(result, "href\\W*=\\W*[\"'].*?[\"']", "href='http://www.google.com'");
-        }
-
-        /// <summary>
-        /// Get html from resource by resource name.
-        /// </summary>
-        private static string GetHtmlFromResource(string name)
-        {
-            var htmlStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
-            if(htmlStream != null)
-            {
-                using (StreamReader sreader = new StreamReader(htmlStream, Encoding.Default))
-                {
-                    return sreader.ReadToEnd();
-                }
-            }
-            return null;
+            doc.PrintPage += (sender, args) => HtmlRender.RenderGdiPlus(args.Graphics, html, args.MarginBounds.Location, args.MarginBounds.Size);
+            doc.Print();
         }
     }
 }
