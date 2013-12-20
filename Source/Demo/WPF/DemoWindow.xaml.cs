ce31cb5e8f4290ae4031d830f0da777bb400da3d
ArthurHub <thekingarthur@gmail.com>
2013-12-20 18:15:55 +0200

    * add missing file

---
+++ Source/Demo/WPF/DemoWindow.xaml.cs
@@ -0,0 +1,30 @@
+// "Therefore those skilled at the unorthodox
+// are infinite as heaven and earth,
+// inexhaustible as the great rivers.
+// When they come to an end,
+// they begin again,
+// like the days and months;
+// they die and are reborn,
+// like the four seasons."
+// 
+// - Sun Tsu,
+// "The Art of War"
+
+using System.Windows;
+
+namespace HtmlRenderer.Demo.WPF
+{
+    /// <summary>
+    /// Interaction logic for DemoWindow.xaml
+    /// </summary>
+    public partial class DemoWindow
+    {
+        public DemoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {}
+    }
+}
