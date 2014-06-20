43a49748949e31d82de46607ae75e83a8c4f8486
ArthurHub <thekingarthur@gmail.com>
2014-06-20 18:04:03 +0300

    * embed references in demo apps so they can run as single exe

--- Source/Demo/WPF/App.xaml.cs
+++ Source/Demo/WPF/App.xaml.cs
@@ -10,11 +10,44 @@
 // - Sun Tsu,
 // "The Art of War"
 
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
 namespace HtmlRenderer.Demo.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
-    { }
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+        }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+            }
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream != null)
+                {
+                    byte[] assemblyRawBytes = new byte[stream.Length];
+                    stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                    return Assembly.Load(assemblyRawBytes);
+                }
+                return null;
+            }
+        }
+    }
 }
--- Source/Demo/WinForms/Program.cs
+++ Source/Demo/WinForms/Program.cs
@@ -11,6 +11,9 @@
 // "The Art of War"
 
 using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace HtmlRenderer.Demo.WinForms
@@ -23,12 +26,37 @@ namespace HtmlRenderer.Demo.WinForms
         [STAThread]
         private static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DemoForm());
 
             // Application.Run(new PerfForm());
             //  PerfForm.Run();
+        }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+            }
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream != null)
+                {
+                    byte[] assemblyRawBytes = new byte[stream.Length];
+                    stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                    return Assembly.Load(assemblyRawBytes);
+                }
+                return null;
+            }
         }
     }
 }
