90cd47ea959717e26bfb6f857e793b446c8e97c0
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-01-11 00:04:57 +0100

    Add demo console application

---
+++ Source/Demo/ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+
+namespace HtmlRenderer.Demo.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Usage: htmltopdf [input-file] [output-file]");
+                return;
+            }
+
+            var pdf = PdfGenerator.GeneratePdf(File.ReadAllText(args[0]), PdfSharp.PageSize.A4);
+            pdf.Save(args[1]);
+        }
+    }
+}
